4777a62b4447a1b196a10597005d67f576e3012c
Loogibot <94766364+Loogibot@users.noreply.github.com>
2022-05-25 17:32:09 -0400

    logic is done, added turn management, damage logic and display, added results and move details to help fill out the UI, just need to work on end game and that should wrap up the game

--- app/src/main/java/com/loogibot/chainfighter/MainActivity.kt
+++ app/src/main/java/com/loogibot/chainfighter/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.loogibot.chainfighter
 
-import android.os.Build.VERSION_CODES.M
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -18,13 +17,16 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var opponentImage: ImageView
     private lateinit var playerImage: ImageView
+
     private lateinit var playerHP: TextView
     private lateinit var opponentHP: TextView
     private lateinit var playerMove: TextView
     private lateinit var opponentMove: TextView
+    private lateinit var Result: TextView
+    private lateinit var moveDetail: TextView
+
     private lateinit var playerHPBar: ProgressBar
     private lateinit var opponentHPBar: ProgressBar
-    private lateinit var Result: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -42,10 +44,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun gameStart() {
-
-//        Toast.makeText(applicationContext, "SELECT A MOVE", Toast.LENGTH_SHORT).show()
-
+        // make from UI elements
         Result = findViewById(R.id.moveResult)
+        moveDetail = findViewById(R.id.move_details)
 
         playerHPBar = findViewById(R.id.playerHP)
         opponentHPBar = findViewById(R.id.opponentHP)
@@ -62,15 +63,19 @@ class MainActivity : AppCompatActivity() {
         dodgeButton = findViewById(R.id.dodgeButton)
         shieldButton = findViewById(R.id.shieldButton)
 
+        // relate buttons to moves
         kickButton.text = kick.name
         punchButton.text = punch.name
         grabButton.text = grab.name
         dodgeButton.text = dodge.name
         shieldButton.text = shield.name
 
-        playerHP.text = "PLAYER HP: " + playerHealth.toString()
-        opponentHP.text = "OPPONENT HP: " + opponentHealth.toString()
+        if (turnManager == 0) {
+            playerHealth = 200
+            opponentHealth = 200
+        }
 
+        // button operation
         kickButton.setOnClickListener {
             drawMoves(kick)
         }
@@ -113,39 +118,68 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.none_image
         }
 
+        playerHP.text = playerHPLabel + playerHealth.toString()
+        opponentHP.text = opponentHPLabel + opponentHealth.toString()
+
         playerImage.setImageResource(drawPlayerMove)
         opponentImage.setImageResource(drawOpponentMove)
 
-        playerMove.text = "YOUR MOVE IS: " + playerChoice.name.uppercase(Locale.getDefault())
+        playerMove.text = "YOUR MOVE IS " + playerChoice.name.uppercase(Locale.getDefault())
         opponentMove.text =
-            "OPPONENT'S MOVE IS: " + opponentChoice.name.uppercase(Locale.getDefault())
+            "OPPONENT'S MOVE IS " + opponentChoice.name.uppercase(Locale.getDefault())
 
         moveCompare(playerChoice, opponentChoice)
-        moveResult()
 
     }
 
     private fun moveResult() {
-        gameStart()
+        turnManager++
+        Toast.makeText(applicationContext, "Turn Number " + turnManager.toString(), Toast.LENGTH_SHORT).show()
+
+        while (playerHealth > 0 || opponentHealth > 0) {
+            gameStart()
+            break
+        }
+        if (playerHealth == 0) {
+            Result.text = getString(R.string.plHP0)
+            gameEnd()
+        }
+        if (opponentHealth == 0) {
+            Result.text = getString(R.string.opHP0)
+            gameEnd()
+        }
     }
 
     private fun moveCompare(player: Move, opponent: Move) {
+
         if (player.name != opponent.name) {
+            // if player is advantageous
             if (player.name in opponent.firstAdv || player.name in opponent.secondAdv) {
-                Result.text = getString(R.string.opponent_damage)
-                opponentHP.text = (opponentHealth - player.damage).toString()
+                Result.text = getString(R.string.opponent_damage) + " -" + player.damage// who takes dam
+                moveDetail.text = opponent.name.uppercase() + isWeakToText + player.name.uppercase()
+                plHPDam += player.damage
+                opponentHealth -= player.damage
+                opponentHP.text = opponentHPLabel + opponentHealth.toString()
 
             }
-            if (opponent.name in player.firstAdv || opponent.name in player.secondAdv) {
-                Result.text = getString(R.string.player_damage)
-                playerHP.text = (playerHealth - opponent.damage).toString()
+            // if opponent is advantageous
+            else if (opponent.name in player.firstAdv || opponent.name in player.secondAdv) {
+                Result.text = getString(R.string.player_damage) + " -" + opponent.damage// who takes dam
+                moveDetail.text = player.name.uppercase() + isWeakToText + opponent.name.uppercase()
+                opHPDam += opponent.damage
+                playerHealth -= opponent.damage
+                playerHP.text = playerHPLabel + playerHealth.toString()
 
             }
         } else {
             Result.text = getString(R.string.cancel)
         }
+        moveResult()
     }
 
+    private fun gameEnd() {
+
+    }
 
     // creates template of all possible moves with name, damage and that move's advantages
     data class Move(
@@ -159,19 +193,20 @@ class MainActivity : AppCompatActivity() {
 
     private val kick = Move("kick", 25, "punch", "shield")
     private val grab = Move("grab", 5, "kick", "shield")
-    private val dodge = Move("dodge", 0, "kick", "grab")
+    private val dodge = Move("dodge", 5, "kick", "grab")
     private val shield = Move("shield", 5, "punch", "dodge")
     private val punch = Move("punch", 15, "grab", "dodge")
 
     private val opponent = "opponent"
-    private var moveString = "press"
-    private val playerHealth = 200
-    private val opponentHealth = 200
+    private val opponentHPLabel = "OPPONENT HP IS "
+    private val playerHPLabel = "PLAYER HP IS "
+    private val isWeakToText = " IS WEAK TO "
 
-    private fun moveAvailable(move: Move): String {
-        moveString = move.name
-        return move.name
-    }
+    private var plHPDam = 0
+    private var opHPDam = 0
+    private var playerHealth = 200
+    private var opponentHealth = 200
+    private var turnManager = 0
 
     private fun randomMoves(player: String): Move {
         val allMoves = listOf(kick, grab, dodge, shield, punch)
