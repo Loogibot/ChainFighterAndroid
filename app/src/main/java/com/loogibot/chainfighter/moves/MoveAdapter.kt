1c669128e95acb3d3ff6f506f240eed63c60f505
Loogibot <94766364+Loogibot@users.noreply.github.com>
2023-01-27 08:09:31 -0500

    cleaned up redundant button names, which fixed the string, also fixed the images by adding them as an int to Move object, and got rid of the when operator, simplifying the drawmoves function. Game is working better, almost as good as when it was one pager. Back then everything was in one place, which made it extremely difficult to fix anything, but things are MUCH more concise now

--- app/src/main/java/com/loogibot/chainfighter/moves/MoveAdapter.kt
+++ app/src/main/java/com/loogibot/chainfighter/moves/MoveAdapter.kt
@@ -1,10 +1,16 @@
 package com.loogibot.chainfighter.moves
 import com.loogibot.chainfighter.R
 
-open class MoveAdapter {
+open class MoveSource {
     val kickStr = R.string.kick.toString()
     val grabStr = R.string.grab.toString()
     val dodgeStr = R.string.dodge.toString()
     val shieldStr = R.string.shield.toString()
     val punchStr = R.string.punch.toString()
+
+    val kickImg = R.drawable.player_kick
+    val grabImg = R.drawable.player_grab
+    val dodgeImg= R.drawable.player_dodge
+    val shieldImg = R.drawable.player_shield
+    val punchImg= R.drawable.player_punch
 }
