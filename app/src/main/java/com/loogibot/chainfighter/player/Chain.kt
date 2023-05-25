6a73a0176fa769b38ab369c3f58fdef4533ac07f
Loogibot <94766364+Loogibot@users.noreply.github.com>
2023-05-25 10:54:45 -0400

    fixed opponent's chain, simplifying the random move chioce, got rid of chain position since its not necessary, move compare is technically working, need to set up move limits and move results, will need to add UI to show what the move comparisons are

--- app/src/main/java/com/loogibot/chainfighter/player/Chain.kt
+++ app/src/main/java/com/loogibot/chainfighter/player/Chain.kt
@@ -1,7 +1,5 @@
 package com.loogibot.chainfighter.player
 
-import android.content.ContentValues.TAG
-import android.util.Log
 import com.loogibot.chainfighter.moves.Move
 
 class Chain {
@@ -9,27 +7,26 @@ class Chain {
     lateinit var secondMove: Move
     lateinit var thirdMove: Move
 
-    var chainPosition = 0
+    var chainCost = 0
     var totalDamage = 0
     var chainList = mutableListOf<Move>()
+    var moveSetStr = ""
 
     fun chainManager(m: Move) {
-        chainList.add(chainPosition, m)
+        chainList.add(m)
 
         firstMove = chainList.first()
         secondMove =
             chainList[if (chainList.lastIndex == 2) 1 else chainList.lastIndex]
         thirdMove = chainList[chainList.lastIndex]
+        moveSetStr += chainList[chainList.lastIndex].name + " "
 
-        Log.v(TAG, firstMove.name + " is the first move in the chain")
-        Log.v(TAG, secondMove.name + " is the second move in the chain")
-        Log.v(TAG, thirdMove.name + " is the third move in the chain")
-
-        Log.v(TAG, chainList.size.toString() + " is the size of the chain")
-        Log.v(TAG, chainList.lastIndex.toString() + " is the last index of the chain")
-        Log.v(TAG, chainPosition.toString() + " is the position of the chain")
-
-        Log.v(TAG, chainList.toString() + " is the chain")
+//        Log.v(TAG, firstMove.name + " is the first move in the chain")
+//        Log.v(TAG, secondMove.name + " is the second move in the chain")
+//        Log.v(TAG, thirdMove.name + " is the third move in the chain")
+//        Log.v(TAG, chainList.size.toString() + " is the size of the chain")
+//        Log.v(TAG, chainList.lastIndex.toString() + " is the last index of the chain")
+//        Log.v(TAG, chainList.toString() + " is the chain")
 
     }
 }
