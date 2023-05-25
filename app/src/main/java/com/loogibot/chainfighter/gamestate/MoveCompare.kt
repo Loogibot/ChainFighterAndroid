7ef1c8f37f5a8c9f04e92a04dfd6af299bc09341
Loogibot <94766364+Loogibot@users.noreply.github.com>
2023-05-25 12:35:59 -0400

    added results UI, now need to separate move comparison for each result, will likely need to rewrite the entire moveCompare and add a results object that handles the final result

--- app/src/main/java/com/loogibot/chainfighter/gamestate/MoveCompare.kt
+++ app/src/main/java/com/loogibot/chainfighter/gamestate/MoveCompare.kt
@@ -1,7 +1,9 @@
 package com.loogibot.chainfighter.gamestate
 
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.loogibot.chainfighter.R
 import com.loogibot.chainfighter.player.Chain
 import com.loogibot.chainfighter.player.Players
 
@@ -14,11 +16,16 @@ fun moveCompare(playerChain: Chain, opponentChain: Chain, uiObj: List<Any>): Str
     val playerHP = uiObj[4] as TextView
     val opponentHP = uiObj[5] as TextView
     val cancel = uiObj.last() as String
+    val firstMoveComparisonResult = uiObj[18] as ImageView
+    val SecondMoveComparisonResult = uiObj[19] as ImageView
+    val ThirdMoveComparisonResult = uiObj[20] as ImageView
+
     var winner = "NO ONE YET"
     var i = 0
 
     playerChain.chainList.forEach {
         if (it != opponentChain.chainList[i]) {
+            //player move is successful
             if (it.name == opponentChain.chainList[i].firstAdv || it.name == opponentChain.chainList[i].secondAdv) {
                 // result will display who, in this case opponent, took how much damage
                 result.text = "${Players.opponent} TOOK ${playerChain.totalDamage} DAMAGE!"
@@ -30,6 +37,9 @@ fun moveCompare(playerChain: Chain, opponentChain: Chain, uiObj: List<Any>): Str
                 opponentHP.text = "${Players.opponentHPLabel} + ${Players.opponentHealth}"
                 opponentHPBar.progress = Players.opponentHealth
 
+                //update results image
+                firstMoveComparisonResult.setImageResource(R.drawable.player_move_win)
+
             } else if (opponentChain.chainList[i].name == it.firstAdv || opponentChain.chainList[i].name == it.secondAdv) {
                 // result will display who, in this case opponent, took how much damage
                 result.text = "${Players.player} TOOK ${opponentChain.totalDamage} DAMAGE!"
@@ -40,8 +50,11 @@ fun moveCompare(playerChain: Chain, opponentChain: Chain, uiObj: List<Any>): Str
                 Players.playerHealth -= opponentChain.chainList[i]!!.damage
                 playerHP.text = "${Players.playerHPLabel} + ${Players.playerHealth}"
                 playerHPBar.progress = Players.playerHealth
+                //update results image
+                firstMoveComparisonResult.setImageResource(R.drawable.opponent_move_win)
             }
         } else {
+            firstMoveComparisonResult.setImageResource(R.drawable.cancel_image)
             result.text = cancel
         }
         i++
