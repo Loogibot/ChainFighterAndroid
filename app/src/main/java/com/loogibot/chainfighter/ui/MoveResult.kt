1a98f66cf6f9734947be8f763f1f04f71167010f
Loogibot <94766364+Loogibot@users.noreply.github.com>
2023-05-27 20:39:13 -0400

    need to get the string of the results

--- app/src/main/java/com/loogibot/chainfighter/ui/MoveResult.kt
+++ app/src/main/java/com/loogibot/chainfighter/ui/MoveResult.kt
@@ -1,9 +1,13 @@
 package com.loogibot.chainfighter.ui
 
-import com.loogibot.chainfighter.R.drawable.*
+import com.loogibot.chainfighter.R.drawable.cancel_image
+import com.loogibot.chainfighter.R.drawable.neutral_result
+import com.loogibot.chainfighter.R.drawable.opponent_move_win
+import com.loogibot.chainfighter.R.drawable.player_move_win
 
 class MoveResult {
     companion object Results {
+
         const val neutralResult = neutral_result
         const val cancelResult = cancel_image
         const val playerMoveWinResult = player_move_win
