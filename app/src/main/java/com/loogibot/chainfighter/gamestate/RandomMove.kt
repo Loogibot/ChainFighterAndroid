03c4201830ed32b301956cb91d398eead5baa8fb
Loogibot <94766364+Loogibot@users.noreply.github.com>
2023-06-14 02:30:43 -0400

    slowly fixing up the ui so that it actually works with different phone sizes, finally got around to limiting the random moves of the opponent, cleaned up some commented out code

--- app/src/main/java/com/loogibot/chainfighter/gamestate/RandomMove.kt
+++ app/src/main/java/com/loogibot/chainfighter/gamestate/RandomMove.kt
@@ -2,8 +2,15 @@ package com.loogibot.chainfighter.gamestate
 
 import com.loogibot.chainfighter.moves.Move
 import com.loogibot.chainfighter.moves.MoveSource.M.m
+import com.loogibot.chainfighter.player.Players
 
 fun randomMove(): Move {
-    val allMoves = listOf(m.kick, m.grab, m.dodge, m.shield, m.punch)
-    return allMoves.random()
+    val allMoves = listOf(m.kick, m.punch, m.grab, m.dodge, m.shield)
+
+    return when (Players.oChain.chainCost) {
+        in 4..5 -> allMoves.subList(1, 4).random()
+        in 4..6 -> allMoves.subList(2, 4).random()
+        in 4..7 -> allMoves.subList(3, 4).random()
+        else -> allMoves.random()
+    }
 }
