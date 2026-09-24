335a33da26b5538d6d98a49fb91788390c99a6e1
Loogibot <luigi_mota89@hotmail.com>
2026-09-24 14:41:35 -0500

    updating and running, will rebuild for dist

--- app/src/main/java/com/loogibot/chainfighter/MainActivity.kt
+++ app/src/main/java/com/loogibot/chainfighter/MainActivity.kt
@@ -19,6 +19,7 @@ import com.loogibot.chainfighter.player.Chain
 import com.loogibot.chainfighter.player.Players
 import com.loogibot.chainfighter.ui.drawMoves
 import java.net.URI
+import androidx.core.net.toUri
 
 open class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -171,7 +172,7 @@ open class MainActivity : AppCompatActivity() {
         ppBinding.viewPrivacyPolicyOnline.setOnClickListener {
             val url = URI("https://luigimota.com/#privacypolicy")
             val i = Intent(Intent.ACTION_VIEW)
-            i.data = Uri.parse(url.toString())
+            i.data = url.toString().toUri()
             startActivity(i)
         }
     }
